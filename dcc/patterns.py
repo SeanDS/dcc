129db7dc7b308bc99bd2539dc45affb61efceeae
Sean Leavey <sean.leavey@gmail.com>
2016-09-16 15:59:50 +0100

    Commenting

--- dcc/patterns.py
+++ dcc/patterns.py
@@ -2,6 +2,10 @@ import re
 import record
 
 class DccPatterns(object):
+    """Handles extraction of useful information from DCC pages.
+    """
+    
+    """DCC document type designators and descriptions"""
     document_type_letters = {
         "C": "Contractual or procurement",
         "D": "Drawings",
@@ -16,100 +20,149 @@ class DccPatterns(object):
         "T": "Techical notes"
     }
     
-    # DCC category and number regex string
+    """DCC category and number regular expression"""
     _regex_str_dcc = "([a-z])(\\d+)"
     
-    # DCC category, number and version regex string (optional version string)
-    # version strings on the DCC are either -vX where X is an integer > 0, or -x0
+    """DCC category, number and version regular expression
+    Version strings on the DCC are either -vX where X is an integer > 0, or -x0
+    """
     _regex_str_full_dcc = "([a-z])(\\d+)(-[vx](\\d+))?"
     
     def __init__(self):
-        # DCC number, of the form "X0000000"
+        """Instantiates a DccPatterns object, compiling some useful regular expressions"""
+        
+        # regex matching DCC category and number in strings of the form "T0000000"
         self._regex_dcc_string = re.compile(self._regex_str_dcc, re.IGNORECASE)
     
-        # DCC number with whitespace either side, of the form "blah-X0000000-blah"
+        # regex matching DCC category and number within a larger string
         self._regex_dcc_mixed_string = re.compile(".*?" + self._regex_str_dcc + ".*?", re.IGNORECASE)
         
-        # DCC number with version, of the form "X0000000-v1"
+        # regex matching DCC category, number and version in strings of the form "T0000000-v5"
         self._regex_full_dcc_string = re.compile(self._regex_str_full_dcc, re.IGNORECASE)
         
-        # DCC number with version, with whitespace either side, of the form "blah-X0000000-blah"
+        # regex matching DCC category, number and version within a larger string
         self._regex_full_dcc_mixed_string = re.compile(".*?" + self._regex_str_full_dcc + ".*?", re.IGNORECASE)
     
     def get_dcc_number_from_string(self, string, mixed=True, require_version=False):
+        """Extracts the DCC number from a string and returns a DccNumber object
+        
+        :param string: string to match DCC number in
+        :param mixed: whether to allow match in a larger string
+        :param require_version: whether to require that a version is designated in the match
+        """
+        
         if mixed:
+            # match pattern within a larger string
             pattern = self._regex_full_dcc_mixed_string
         else:
+            # match exact pattern
             pattern = self._regex_full_dcc_string
         
+        # search for matches and pass them to another function to validate and create the object; return this object
         return self._dcc_number_from_search(pattern.search(string), require_version=require_version)
 
     def is_dcc_number(self, dcc_str):
-        # match both type and number
-        search = self._regex_dcc_string.search(dcc_str)
+        """Checks if the specified string is a valid DCC number
         
+        :param dcc_str: the string to check
+        """
+        
+        # search for an exact DCC number, catching a non-match
         try:
-            self._dcc_number_from_search(search)
-        except DccNumberNotFoundException as e:
+            self.get_dcc_number_from_string(dcc_str, mixed=False)
+        catch InvalidDccNumberException as e:
+            # exception is thrown for a non-match, so return false
             return False
         
         return True
     
-    def _dcc_number_from_search(self, regex_search, require_version=False):        
-        # NoneType indicates no matches
+    def _dcc_number_from_search(self, regex_search, require_version=False):
+        """Validates the matched values in a regular expression search for a DCC number in a string
+        
+        :param regex_search: search results object from regular expression
+        :param require_version: whether to require that a version is specified in the matched string
+        """
+        
+        # if the regex search is NoneType, that means no valid values were found
         if regex_search is None:
             raise DccNumberNotFoundException()
         
         # extract group
         group = regex_search.groups()
         
+        # first match is the category
         category_letter = group[0]
+        
+        # second match is the number
         dcc_numeric = group[1]
         
-        # validate letter designator
+        # check category is valid
         if not self.is_category_letter(category_letter):
             raise InvalidDccNumberException()
         
-        # validate numeric
+        # check number is valid
         if not self.is_dcc_numeric(dcc_numeric):
             raise InvalidDccNumberException()
         
-        # find optional version
-        # group[2] is the matched -v1 string, but we want the 4th match representing the version number itself
+        # find version, if present
+        # group[2] is the matched "-v1" string, but we want the actual number, which is the fourth match
         version = group[3]
         
-        # check if version is required
+        # if the version is NoneType, and we require it, then throw an exception
         if require_version and version is None:
             raise VersionNotFoundInSearch()
         
+        # try to parse the version if it was matched
         if version is not None:
-            # validate version since it was found
+            # check if version number is valid
             if not self.is_dcc_version(version):
                 raise InvalidVersionException()
         
+        # return a new DccNumber object representing the matched information
         return record.DccNumber(category_letter, dcc_numeric, version)
     
     @classmethod
     def is_category_letter(cls, letter):
+        """Checks if the specified category letter is valid
+        
+        :param letter: category letter to check
+        """
+        
+        # check if letter is in list of valid letters
         return letter in cls.document_type_letters.keys()
     
     @classmethod
     def is_dcc_numeric(cls, numeric):
-        # TODO: add specific constraints
+        """Checks if the specified number is a valid DCC numeral
+        
+        :param numeric: DCC numeral to check
+        """
+        
+        # for now, just check if the number is a positive integer
+        # TODO: any other constraints to check, e.g. length?
         return int(numeric) > 0
     
     @classmethod
     def is_dcc_version(cls, version):
+        """Checks if the specified version number is valid
+        
+        :param version: version to check
+        """
+        
         return int(version) >= 0
 
 class DccNumberNotFoundException(Exception):
+    """Exception for when a DCC number is not found"""
     pass
 
 class InvalidDccNumberException(Exception):
+    """Exception for when a DCC number is invalid"""
     pass
 
 class VersionNotFoundInSearch(Exception):
+    """Exception for when a version is not found"""
     pass
 
 class InvalidVersionException(Exception):
+    """Exception for when a version is invalid"""
     pass
--- dcc/record.py
+++ dcc/record.py
@@ -3,81 +3,139 @@ from bs4 import BeautifulSoup as bs
 import patterns
 
 class DccNumber(object):
+    """Represents a DCC number, including category, numeric and version"""
+    
     def __init__(self, category, numeric, version=None):
+        """Instantiates a DccNumber object
+        
+        :param category: DCC category
+        :param numeric: DCC record number
+        :param version: DCC record version
+        """
+        
+        # check that the category is valid
         if not patterns.DccPatterns.is_category_letter(category):
             raise Exception("Invalid category letter")
         
+        # check that the DCC number is valid
         if not patterns.DccPatterns.is_dcc_numeric(numeric):
             raise Exception("Invalid numeric")
         
-        if version is not None:
-            version = int(version)
-            
+        # check that the version is valid, if it is specified
+        if version is not None:            
             if not patterns.DccPatterns.is_dcc_version(version):
                 raise Exception("Invalid version")
         
+        # set the values
         self.category = category
         self.numeric = numeric
         self.version = version
     
     @classmethod
-    def init_from_num(cls, dcc_num):
+    def init_from_num(cls, dcc_str):
+        """Instantiate a DccNumber object from a DCC string
+        
+        :param dcc_str: string presenting DCC designator
+        """
+        
+        # create a pattern matcher object
         dcc_patterns = patterns.DccPatterns()
         
-        return dcc_patterns.get_dcc_number_from_string(dcc_num, mixed=False)
+        # match the DCC number and return a new object representing it
+        return dcc_patterns.get_dcc_number_from_string(dcc_str, mixed=False)
     
     def get_version_str(self):
+        """Get the version suffix"""
+        
+        # when no version is known, use a question mark
         if self.version is None:
             return "v?"
+        # when the version is zero, use an x (consistent with DCC)
         elif self.version is 0:
             return "x0"
         
+        # return the formatted version string
         return "v{0}".format(self.version)
     
     def __repr__(self):
+        """String representation of the DCC number"""
         return "{0}{1}-{2}".format(self.category, self.numeric, self.get_version_str())
 
 class DccRecord(object):
+    """Represents a DCC record"""
+    
     def __init__(self, dcc_number):
+        """Instantiates a DCC record
+        
+        :param dcc_number: DCC number object representing the record
+        """
+        
         self.dcc_number = dcc_number
         
         # default empty content
         self.content = None
     
     def has_content(self):
+        """Checks if the record content has been downloaded"""
+        
         return self.content is not None
     
     def set_content(self, content):
+        """Sets the record content"""
+        
         self.content = content
     
     @classmethod
-    def init_from_page(cls, page):
-        # patterns object
+    def init_from_page(cls, content):
+        """Instantiates a DccRecord given page content
+        
+        :param content: page content to set
+        """
+        
+        # create a pattern matcher object
         dcc_patterns = patterns.DccPatterns()
         
-        navigator = cls._get_html_navigator(page)
+        # get a navigator object for the content
+        navigator = cls._get_html_navigator(content)
         
-        # get dcc number using page title
+        # get dcc number using content title
         dcc_number = cls._extract_full_dcc_number_from_page_title(navigator, dcc_patterns)
         
+        # instantiate and return the object given the DCC number
         return cls(dcc_number)
     
     @classmethod
     def _extract_full_dcc_number_from_page_title(cls, navigator, dcc_patterns):
-        # find title h1
-        title = navigator.find(id='title')
+        """Extracts the DCC number with version from the DCC record page's title
         
-        if title.name != 'h1':
-            raise UnexpectedHtmlElementException()
+        :param navigator: navigator object to search document
+        :param dcc_patterns: patterns object to match DCC number
+        """
         
-        # get DCC number
+        # find title
+        title = navigator.find("h1", id='title')
+        
+        # make sure it was found
+        if title is None:
+            raise DccRecordTitleNotFoundException()
+        
+        # get and return DCC number
         return dcc_patterns.get_dcc_number_from_string(title.string, mixed=True, require_full=True)
 
     @classmethod
-    def _get_html_navigator(cls, page_content):
-        return bs(page_content, 'html.parser')
+    def _get_html_navigator(cls, content):
+        """Creates a navigator object for the specified HTML document
+        
+        :param content: content to navigate
+        """
+        
+        # create and return a BeautifulSoup object
+        return bs(content, 'html.parser')
     
     def _get_content_navigator(self):
+        """Gets a navigator object for the page content"""
+        
+        # make sure the page content has been downloaded
         if not self.has_content():
             raise Exception("Page content not yet downloaded")
         
@@ -85,13 +143,17 @@ class DccRecord(object):
         return self._get_html_navigator(self.content)
     
     def _extract_versions(self):
+        """Extract the version from the page content"""
+        
+        # get a navigator object for the record
         navigator = self._get_content_navigator()
         
         # get div containing other versions
-        versions_div = navigator.find(id='OtherVersions')
+        versions_div = navigator.find("div", id='OtherVersions')
         
-        if versions_div.name != 'div':
-            raise UnexpectedHtmlElementException()
+        # check it was found
+        if versions_div is None:
+            raise DccRecordTitleNotFoundException()
         
         # create dcc pattern matcher
         dcc_patterns = patterns.DccPatterns()
@@ -100,23 +162,38 @@ class DccRecord(object):
         return [dcc_patterns.get_dcc_number_from_string(tag['title'], require_version=True) for tag in versions_div.find_all('a')]
     
     def _extract_files(self):
+        """Extract a list of files within this DCC record"""
+        
+        # get a navigator object for the record
         navigator = self._get_content_navigator()
         
         # get files lists
         files_classes = navigator.find_all("dd", class_="FileList")
         
+        # empty files list
         files = []
         
+        # loop over found classes, searching for URLs and creating corresponding DccFile objects in the list
         for files_class in files_classes:
             files.extend([DccFile(url_tag.string, url_tag['title'], url_tag['href']) for url_tag in files_class.find_all("a")])
         
+        # return list of DccFile objects
         return files
 
 class DccFile(object):
+    """Represents a file attached to a DCC document"""
+    
     def __init__(self, title, filename, url):
+        """Instantiates a DCC file object
+        
+        :param title: file title
+        :param filename: filename
+        :param url: file URL string
+        """
+        
         self.title = title
         self.filename = filename
         self.url = url
 
-class UnexpectedHtmlElementException(Exception):
+class DccRecordTitleNotFoundException(Exception):
     pass
