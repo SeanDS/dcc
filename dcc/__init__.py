7b992c9eb38b3200abbafd13840aedb993732469
Sean Leavey <gpg@attackllama.com>
2022-01-13 11:36:31 +0100

    Add package metadata; some housekeeping

--- dcc/__init__.py
+++ dcc/__init__.py
@@ -1,8 +1,16 @@
-# -*- coding: utf-8 -*-
+"""Tools for interactive and programmatic access to the LIGO DCC."""
 
-"""DCC API"""
+import locale
 
-import logging
+PROGRAM = __name__
+DESCRIPTION = "Tools for interactive and programmatic access to the LIGO DCC."
 
-# add a null handler to the root to suppress warnings about no handler
-logging.getLogger().addHandler(logging.NullHandler())
+# Set the locale to the user's default (required for e.g. number formatting in log
+# warnings).
+locale.setlocale(locale.LC_ALL, "")
+
+# Get package version.
+try:
+    from .version import version as __version__
+except ImportError:
+    raise Exception("Could not find version.py. Ensure you have run setup.")
