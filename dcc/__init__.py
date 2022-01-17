7ef33538a7083407e7b992dc6687b7c62b688b4b
Sean Leavey <gpg@attackllama.com>
2022-01-17 21:43:03 +0100

    Remove unnecessary locale setting

--- dcc/__init__.py
+++ dcc/__init__.py
@@ -1,14 +1,8 @@
 """Tools for interactive and programmatic access to the LIGO DCC."""
 
-import locale
-
 PROGRAM = __name__
 AUTHORS = ["Sean Leavey", "Jameson Graef Rollins"]
 PROJECT_URL = "https://git.ligo.org/sean-leavey/dcc/"
-
-# Set the locale to the user's default (required for e.g. number formatting in log
-# warnings).
-locale.setlocale(locale.LC_ALL, "")
 
 # Get package version.
 try:
